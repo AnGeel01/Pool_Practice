e17a017caf9b1d4b0755084f3fcb647f7ea254ca
laksouriyahya@gmail.com <laksouriyahya@gmail.com>
2025-07-19 16:45:19 +0100

    C01

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -12,4 +12,17 @@ void ft_rev_int_tab(int *tab, int size)
 		a++;
 		b--;
 	}
-}	
+}
+
+/*int main()
+{
+	int tab[] = {13, 23, 99, 10};
+	ft_rev_int_tab(tab, 4);
+	
+	int i = 0;
+	while(i < 4)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+}*/
