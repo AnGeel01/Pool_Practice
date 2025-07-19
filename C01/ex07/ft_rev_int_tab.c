a25d803df87a13dad9e71051dda7423c1410551f
laksouriyahya@gmail.com <laksouriyahya@gmail.com>
2025-07-19 16:09:20 +0100

    C00

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,13 +1,15 @@
 void ft_rev_int_tab(int *tab, int size)
 {
-	int i, temp;
+	int a = 0;
+	int b = size - 1;
+	int temp;
 
-	i = 0;
-	while(i < size / 2)
+	while(a < size / 2)
 	{
-		temp = tab[i];
-		tab[i] = tab[size - 1 - i];
-		tab[size - 1 - i] = temp;
-		i++;
-	}	
-}
+		temp = tab[a];
+		tab[a] = tab[b];
+		tab[b] = temp;
+		a++;
+		b--;
+	}
+}	
